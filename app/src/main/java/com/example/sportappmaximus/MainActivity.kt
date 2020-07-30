9c817ef763532318bdba6e69592662f530efc209
Assasinnys <dmitry.zenevich@yandex.by>
2020-07-30 16:41:35 +0300

    init: upload project base

---
+++ app/src/main/java/com/example/sportappmaximus/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sportappmaximus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
