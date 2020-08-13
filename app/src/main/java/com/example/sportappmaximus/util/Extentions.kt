24e0f62acaa0f014061f664bd1edaa05b0393996
Assasinnys <dmitry.zenevich@yandex.by>
2020-08-13 17:59:47 +0300

    feat: add chat window fragment

--- app/src/main/java/com/example/sportappmaximus/util/Extentions.kt
+++ app/src/main/java/com/example/sportappmaximus/util/Extentions.kt
@@ -1,6 +1,10 @@
 package com.example.sportappmaximus.util
 
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.sportappmaximus.MainActivity
+import com.example.sportappmaximus.adapter.ChatRecyclerViewAdapter
 
 fun Fragment.mainActivity() = requireActivity() as MainActivity
+
+fun RecyclerView.chatAdapter() = adapter as ChatRecyclerViewAdapter
