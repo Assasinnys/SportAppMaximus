777250c807a20efc7873df3e311d02114e2114fb
Assasinnys <dmitry.zenevich@yandex.by>
2020-08-23 01:18:27 +0300

    fix: merge errors

--- app/src/main/java/com/example/sportappmaximus/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/sportappmaximus/ui/settings/SettingsFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import com.example.sportappmaximus.R
-import kotlinx.android.synthetic.main.fragment_settings.*
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
@@ -14,8 +12,6 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
-            tv_settings.text = it
-        })
+
     }
 }
