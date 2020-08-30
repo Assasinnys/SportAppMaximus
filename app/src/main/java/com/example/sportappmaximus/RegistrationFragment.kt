61b1145eb25263c4f413c78664ac7d886659f86f
Assasinnys <dmitry.zenevich@yandex.by>
2020-08-30 03:40:28 +0300

    fix: registration main screen layout

--- app/src/main/java/com/example/sportappmaximus/RegistrationFragment.kt
+++ app/src/main/java/com/example/sportappmaximus/RegistrationFragment.kt
@@ -1,6 +1,18 @@
 package com.example.sportappmaximus
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_registration_main.*
 
-class RegistrationFragment : Fragment(R.layout.fragment_user_registration) {
+class RegistrationFragment : Fragment(R.layout.fragment_registration_main) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        img_btn_trainer.setOnClickListener {
+            //TODO navigate to trainer registration fragment
+        }
+        img_btn_user.setOnClickListener {
+            //TODO navigate to user registration fragment
+        }
+    }
 }
