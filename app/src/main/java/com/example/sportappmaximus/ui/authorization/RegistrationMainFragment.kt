c62711fd997ae4bf8b6c4fba4c152861a9afdd2e
Assasinnys <dmitry.zenevich@yandex.by>
2020-08-31 02:22:56 +0300

    feat: setup RegistrationMainFragment.kt

--- app/src/main/java/com/example/sportappmaximus/ui/authorization/RegistrationMainFragment.kt
+++ app/src/main/java/com/example/sportappmaximus/ui/authorization/RegistrationMainFragment.kt
@@ -3,17 +3,20 @@ package com.example.sportappmaximus.ui.authorization
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.sportappmaximus.R
 import kotlinx.android.synthetic.main.fragment_registration_main.*
 
 class RegistrationMainFragment : Fragment(R.layout.fragment_registration_main) {
 
+    private val navController by lazy { findNavController() }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         img_btn_trainer.setOnClickListener {
-            //TODO navigate to trainer registration fragment
+            navController.navigate(R.id.action_registration_main_fragment_to_registration_trainer_fragment)
         }
         img_btn_user.setOnClickListener {
-            //TODO navigate to user registration fragment
+            navController.navigate(R.id.action_registration_main_fragment_to_registration_user_fragment)
         }
     }
 }
