7de5b5860a75e2fd8275a97d7243b84ea641a726
Assasinnys <dmitry.zenevich@yandex.by>
2020-09-01 23:49:49 +0300

    refactor: code cleanup

--- app/src/main/java/com/example/sportappmaximus/ui/chat/ChatFragment.kt
+++ app/src/main/java/com/example/sportappmaximus/ui/chat/ChatFragment.kt
@@ -2,11 +2,9 @@ package com.example.sportappmaximus.ui.chat
 
 import android.os.Bundle
 import android.view.View
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.sportappmaximus.R
-import com.example.sportappmaximus.util.CHAT_TYPE_KEY
 import com.example.sportappmaximus.util.mainActivity
 import kotlinx.android.synthetic.main.fragment_chat.*
 
@@ -18,7 +16,7 @@ class ChatFragment : Fragment(R.layout.fragment_chat) {
 
         img_btn_trainer_chat.setOnClickListener {
             findNavController().navigate(
-            R.id.action_navigation_chat_to_trainersChatListWindowScreen
+                R.id.action_navigation_chat_to_trainersChatListWindowScreen
             )
         }
         img_btn_user_chat.setOnClickListener {
