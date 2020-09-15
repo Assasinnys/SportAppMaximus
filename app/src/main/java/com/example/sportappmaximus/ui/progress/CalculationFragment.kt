4d4327bd501fc244fcd191dad3b11c2726e3b47e
vkopkov <vkopkov@intervale.ru>
2020-09-15 17:20:36 +0300

    fix oll

---
+++ app/src/main/java/com/example/sportappmaximus/ui/progress/CalculationFragment.kt
@@ -0,0 +1,8 @@
+package com.example.sportappmaximus.ui.progress
+
+import androidx.fragment.app.Fragment
+import com.example.sportappmaximus.R
+
+class CalculationFragment : Fragment(R.layout.fragment_member_account) {
+
+}
