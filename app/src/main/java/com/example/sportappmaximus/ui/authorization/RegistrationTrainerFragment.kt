e19ade863183e5c81eac2c8d6c94789e563245e1
Assasinnys <dmitry.zenevich@yandex.by>
2020-09-15 17:43:39 +0300

    feat: add click listener for account verify button 2

--- app/src/main/java/com/example/sportappmaximus/ui/authorization/RegistrationTrainerFragment.kt
+++ app/src/main/java/com/example/sportappmaximus/ui/authorization/RegistrationTrainerFragment.kt
@@ -1,10 +1,13 @@
 package com.example.sportappmaximus.ui.authorization
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.sportappmaximus.R
+import com.example.sportappmaximus.util.URL_VERIFICATION
 import com.example.sportappmaximus.util.areFieldsValid
 import com.example.sportappmaximus.util.isPasswordsEquals
 import kotlinx.android.synthetic.main.fragment_registration_trainer.*
@@ -26,6 +29,14 @@ class RegistrationTrainerFragment : Fragment(R.layout.fragment_registration_trai
                 ) && isPasswordsEquals(tf_input_pass_trainer, tf_input_pass_conf_trainer)
             )
                 navController.navigate(R.id.action_registration_trainer_fragment_to_navigation_home)
+        }
+
+        btn_confirm.setOnClickListener {
+            val browseIntent = Intent(Intent.ACTION_VIEW)
+            browseIntent.data = Uri.parse(URL_VERIFICATION)
+            if (browseIntent.resolveActivity(requireContext().packageManager) != null) {
+                startActivity(browseIntent)
+            }
         }
     }
 
