3012c0e53b0a1c53901d473a52d16a49de04bc9c
Assasinnys <dmitry.zenevich@yandex.by>
2020-09-15 17:37:28 +0300

    feat: add click listener for account verify button

--- app/src/main/java/com/example/sportappmaximus/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/sportappmaximus/ui/settings/SettingsFragment.kt
@@ -1,10 +1,14 @@
 package com.example.sportappmaximus.ui.settings
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.sportappmaximus.R
+import com.example.sportappmaximus.util.URL_VERIFICATION
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
@@ -12,6 +16,12 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        btn_confirm.setOnClickListener {
+            val browseIntent = Intent(Intent.ACTION_VIEW)
+            browseIntent.data = Uri.parse(URL_VERIFICATION)
+            if (browseIntent.resolveActivity(requireContext().packageManager) != null) {
+                startActivity(browseIntent)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/sportappmaximus/util/Constants.kt
+++ app/src/main/java/com/example/sportappmaximus/util/Constants.kt
@@ -5,6 +5,7 @@ const val NAME = "name"
 const val SURNAME = "surname"
 const val BASE_URL = "http://10.2.84.14:8080"
 const val GOODS_ENDPOINT = "/api/goods"
+const val URL_VERIFICATION = "http://10.2.84.14:8081/app/verify/1"
 
 const val CATEGORY_KEY = "category"
 const val CATEGORY_SPORTSWEAR = 1
