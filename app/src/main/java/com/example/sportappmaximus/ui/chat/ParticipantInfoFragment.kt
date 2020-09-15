b2aa9a62c603734fd7d2701b734fa39cedd3d158
Assasinnys <dmitry.zenevich@yandex.by>
2020-09-15 17:20:47 +0300

    fix: coach and posos info photo

--- app/src/main/java/com/example/sportappmaximus/ui/chat/ParticipantInfoFragment.kt
+++ app/src/main/java/com/example/sportappmaximus/ui/chat/ParticipantInfoFragment.kt
@@ -1,7 +1,17 @@
 package com.example.sportappmaximus.ui.chat
 
 import androidx.fragment.app.Fragment
+import coil.api.load
+import coil.transform.CircleCropTransformation
 import com.example.sportappmaximus.R
+import kotlinx.android.synthetic.main.fragment_member_account.*
 
 class ParticipantInfoFragment : Fragment(R.layout.fragment_member_account) {
+
+    override fun onStart() {
+        super.onStart()
+        avatarImageView.load(R.drawable.p1) {
+            transformations(CircleCropTransformation())
+        }
+    }
 }
--- app/src/main/java/com/example/sportappmaximus/ui/chat/TrainerInfoFragment.kt
+++ app/src/main/java/com/example/sportappmaximus/ui/chat/TrainerInfoFragment.kt
@@ -1,7 +1,17 @@
 package com.example.sportappmaximus.ui.chat
 
 import androidx.fragment.app.Fragment
+import coil.api.load
+import coil.transform.CircleCropTransformation
 import com.example.sportappmaximus.R
+import kotlinx.android.synthetic.main.fragment_coach_account.*
 
 class TrainerInfoFragment : Fragment(R.layout.fragment_coach_account) {
+
+    override fun onStart() {
+        super.onStart()
+        avatarImageView.load(R.drawable.t1) {
+            transformations(CircleCropTransformation())
+        }
+    }
 }
