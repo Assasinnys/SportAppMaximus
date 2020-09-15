5f21cc31f50637fb6c7c3abb7faf4115a82caa3e
Assasinnys <dmitry.zenevich@yandex.by>
2020-09-15 16:10:46 +0300

    feat: add progress screen logic

--- app/src/main/java/com/example/sportappmaximus/ui/progress/ProgressFragment.kt
+++ app/src/main/java/com/example/sportappmaximus/ui/progress/ProgressFragment.kt
@@ -1,10 +1,14 @@
 package com.example.sportappmaximus.ui.progress
 
+import android.content.Intent
+import android.content.Intent.EXTRA_TEXT
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.sportappmaximus.R
+import kotlinx.android.synthetic.main.fragment_progress.*
 
 class ProgressFragment : Fragment(R.layout.fragment_progress) {
 
@@ -12,5 +16,15 @@ class ProgressFragment : Fragment(R.layout.fragment_progress) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        iv_stats.setOnClickListener {
+            findNavController().navigate(R.id.action_navigation_progress_to_progress_screen_fragment)
+        }
+        btn_share.setOnClickListener {
+            val sendIntent = Intent(Intent.ACTION_SEND)
+            sendIntent.putExtra(EXTRA_TEXT, "This is a test message")
+            sendIntent.type = "text/plain"
+            if (sendIntent.resolveActivity(requireContext().packageManager) != null)
+                startActivity(sendIntent)
+        }
     }
 }
--- app/src/main/java/com/example/sportappmaximus/ui/progress/ProgressViewModel.kt
+++ app/src/main/java/com/example/sportappmaximus/ui/progress/ProgressViewModel.kt
@@ -1,13 +1,6 @@
 package com.example.sportappmaximus.ui.progress
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ProgressViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is Progress Fragment"
-    }
-    val text: LiveData<String> = _text
 }
