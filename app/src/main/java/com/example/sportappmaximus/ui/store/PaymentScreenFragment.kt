8279362b60462bc17501dcc07fd84f80ff836f84
Assasinnys <dmitry.zenevich@yandex.by>
2020-09-16 00:21:38 +0300

    fix: add click listener for cancel button

--- app/src/main/java/com/example/sportappmaximus/ui/store/PaymentScreenFragment.kt
+++ app/src/main/java/com/example/sportappmaximus/ui/store/PaymentScreenFragment.kt
@@ -1,8 +1,18 @@
 package com.example.sportappmaximus.ui.store
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.sportappmaximus.R
+import kotlinx.android.synthetic.main.fragment_payment_screen.*
 
 class PaymentScreenFragment : Fragment(R.layout.fragment_payment_screen) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        button3.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
 
 }
