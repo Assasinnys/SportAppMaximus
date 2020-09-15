f5ce4f116acb1138835df0a1219bff11a11d91a3
vkopkov <vkopkov@intervale.ru>
2020-09-15 23:40:37 +0300

    payment fragment

---
+++ app/src/main/java/com/example/sportappmaximus/ui/store/PaymentScreenFragment.kt
@@ -0,0 +1,8 @@
+package com.example.sportappmaximus.ui.store
+
+import androidx.fragment.app.Fragment
+import com.example.sportappmaximus.R
+
+class PaymentScreenFragment : Fragment(R.layout.fragment_payment_screen) {
+
+}
